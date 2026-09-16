377cb4749aaf69dbd1fa080fad47206823901f71
JoacoBessonart <joacob2007@gmail.com>
2026-09-16 09:45:16 -0300

    primeros 2 tests

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -4,15 +4,28 @@ namespace Ucu.Poo.RolePlayGame.Tests
 {
     public class TestClass
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void GetAttackValue_InventoryWithAxeAndSword_ReturnsTotalAttack()
         {
+            Inventory inventory = new Inventory();
+            inventory.AddAxe(new Axe(20));
+            inventory.AddSword(new Sword(15));
+
+            Assert.That(inventory.GetAttackValue(), Is.EqualTo(35));
         }
 
         [Test]
-        public void TestMethod()
+        public void Cure_InjuredCharacter_RestoresInitialHealth()
         {
-            Assert.Pass();
+            Dwarve character = new Dwarve("Gimli");
+            Dwarve attacker = new Dwarve("Thorin");
+            attacker.Inventory.AddAxe(new Axe(30));
+            character.ReceiveAttack(attacker);
+            Assert.That(character.Health, Is.EqualTo(70));
+
+            character.Cure();
+
+            Assert.That(character.Health, Is.EqualTo(character.InitialHealth));
         }
     }
 }
